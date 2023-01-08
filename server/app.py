f494764a20803526dd58c9f9f7da04fe7076e50c
Moore <moore_li4@outlook.com>
2023-01-08 08:48:50 -0500

    yes

--- server/app.py
+++ server/app.py
@@ -26,14 +26,11 @@ def autocomplete():
                options.append(state)
          
          return jsonify(options)
+      elif req["type"] == "county":
+         return jsonify(counties_and_cities[state_unabbrev[req["state"]]])
    else:
       return render_template('index.html')
 
-
-
-   
-cities_and_counties = {}
-#state
 state_abbrev = {
     'AK': 'Alaska',
     'AL': 'Alabama',
@@ -141,30 +138,93 @@ state_unabbrev = {
     "Wyoming": "WY",
     "District of Columbia": "DC"}
 
+#crime rate
+state_crime_abbrev = {
+   #  'AK': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Alaska_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'AL': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Alabama_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'AR': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Arkansas_Offense_Type_by_Agency_2021.xlxs")).active,
+   #  'AZ': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Arizona_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'CA': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/California_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'CO': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Colorado_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'CT': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Connecticut_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'DC': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/District_of_Columbia_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'DE': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Delaware_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'FL': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Florida_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'GA': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Georgia_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'HI': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Hawaii_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'IA': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Iowa_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'ID': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Idaho_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'IL': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Illinois_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'IN': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Indiana_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'KS': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Kansas_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'KY': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Kentucky_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'LA': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Loisiana_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'MA': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Massachusetts_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'MD': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Maryland_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'ME': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Maine_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'MI': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Michigan_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'MN': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Minnesota_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'MO': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Missouri_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'MS': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Mississippi_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'MT': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Montana_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'NC': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/North_Carolina_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'ND': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/North_Dakota_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'NE': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Nebraska_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'NH': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/New_Hampshire_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'NJ': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/New_Jersey_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'NM': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/New_Mexico_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'NV': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Nevada_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'NY': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/New_York_Offense_Type_by_Agency_2021.xlsx")).active,
+    'OH': load_workbook(os.path.abspath("State Tables Offenses by Agency/Ohio_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'OK': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Oklahoma_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'OR': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Oregon_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'PA': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Pennsylvania_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'RI': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Rhode_Island_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'SC': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/South_Carolina_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'SD': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/South_Dakota_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'TN': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Tennessee_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'TX': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Texas_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'UT': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Utah_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'VA': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Virginia_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'VT': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Vermont_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'WA': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Washington_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'WI': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Wisconsin_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'WV': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/West_Virginia_Offense_Type_by_Agency_2021.xlsx")).active,
+   #  'WY': load_workbook(os.path.abspath("server/State Tables Offenses by Agency/Wyoming_Offense_Type_by_Agency_2021.xlsx")).active
+    
+}
+
+
 
 #rent prices
-median_rent_prices_of_counties_spreadsheet = load_workbook(os.path.abspath("FY2023_FMR_50_county.xlsx")).active #find spreadsheet with county, OH
+median_rent_prices_of_counties_spreadsheet = load_workbook(os.path.abspath("FY2023_FMR_50_county.xlsx")).active
+global county_dictionary 
 county_dictionary = {}
 for i in range(2, 4766):
+   x = median_rent_prices_of_counties_spreadsheet.cell(row=i, column=4).value + median_rent_prices_of_counties_spreadsheet.cell(row=i, column=6).value[(median_rent_prices_of_counties_spreadsheet.cell(row=i, column=6).value).find(","):(median_rent_prices_of_counties_spreadsheet.cell(row=i, column=6).value).find(",")+4]
    if ("County" in median_rent_prices_of_counties_spreadsheet.cell(row=i, column=4).value):
-      if not median_rent_prices_of_counties_spreadsheet.cell(row=i, column=4).value + median_rent_prices_of_counties_spreadsheet.cell(row=i, column=6).value[(median_rent_prices_of_counties_spreadsheet.cell(row=i, column=6).value).find(","):(median_rent_prices_of_counties_spreadsheet.cell(row=i, column=6).value).find(",")+4] in county_dictionary.keys():
-         county_dictionary[median_rent_prices_of_counties_spreadsheet.cell(row=i, column=4).value + median_rent_prices_of_counties_spreadsheet.cell(row=i, column=6).value[(median_rent_prices_of_counties_spreadsheet.cell(row=i, column=6).value).find(","):(median_rent_prices_of_counties_spreadsheet.cell(row=i, column=6).value).find(",")+4]] = {}
-      county_dictionary[median_rent_prices_of_counties_spreadsheet.cell(row=i, column=4).value + median_rent_prices_of_counties_spreadsheet.cell(row=i, column=6).value[(median_rent_prices_of_counties_spreadsheet.cell(row=i, column=6).value).find(","):(median_rent_prices_of_counties_spreadsheet.cell(row=i, column=6).value).find(",")+4]]["Median Rent Price"] = median_rent_prices_of_counties_spreadsheet.cell(row=i, column = 8).value
+      if not x in county_dictionary.keys():
+         county_dictionary[x] = {}
+      county_dictionary[x]["Median Rent Price"] = median_rent_prices_of_counties_spreadsheet.cell(row=i, column = 8).value
 
 #house prices
-#median_house_prices_of_counties_spreadsheet = load_workbook(os.path.abspath("server/2022-q3-county-median-prices-and-monthly-mortgage-payment-by-price-12-20-2022.xlsx"))
-#for i in range(2, 4000):
-   
-#crime rate
-
+median_house_prices_of_counties_spreadsheet = load_workbook(os.path.abspath("2022-q3-county-median-prices-and-monthly-mortgage-payment-by-price-12-20-2022.xlsx")).active
+for i in range(2, 3119):
+   x = median_house_prices_of_counties_spreadsheet.cell(row=i, column = 1).value[:median_house_prices_of_counties_spreadsheet.cell(row=i, column = 1).value.find(",")+2] + state_unabbrev[median_house_prices_of_counties_spreadsheet.cell(row=i, column = 1).value[median_house_prices_of_counties_spreadsheet.cell(row=i, column = 1).value.find(",")+3:]]
+   if ("County" in median_house_prices_of_counties_spreadsheet.cell(row=i, column=1).value):
+      if not x in county_dictionary.keys():
+         county_dictionary[x] = {}
+      county_dictionary[x]["Median House Price"] = median_house_prices_of_counties_spreadsheet.cell(row=i, column = 2).value
 
 #population
-
-
-#education report
-
-
-
+county_population_spreadsheet = load_workbook(os.path.abspath("co-est2021-pop.xlsx")).active
+for i in range(6, 3148):
+   x = county_population_spreadsheet.cell(row=i, column = 1).value[:county_population_spreadsheet.cell(row=i, column = 1).value.find(",")+2] + state_unabbrev[county_population_spreadsheet.cell(row=i, column = 1).value[county_population_spreadsheet.cell(row=i, column = 1).value.find(",")+2:]]
+   x = x[1:]
+   if ("County" in county_population_spreadsheet.cell(row=i, column=1).value):
+      if not x in county_dictionary.keys():
+         county_dictionary[x] = {}
+      county_dictionary[x]["Population"] = county_population_spreadsheet.cell(row=i, column = 4).value
 
 #county
 counties_in_states = {}
@@ -174,7 +234,6 @@ for i in range(2, 3145):
       if not state_unabbrev[counties_spreadsheet.cell(row=i, column=3).value] in counties_in_states.keys(): 
          counties_in_states[state_unabbrev[counties_spreadsheet.cell(row=i, column=3).value]] = []
       counties_in_states[state_unabbrev[counties_spreadsheet.cell(row=i, column=3).value]].append(counties_spreadsheet.cell(row=i, column=2).value)
-#print(counties_in_states)
 
 #city
 cities_in_states = {}
@@ -183,13 +242,94 @@ for i in range(2, 30411):
    if not cities_spreadsheet.cell(row=i, column=3).value in cities_in_states.keys(): 
       cities_in_states[cities_spreadsheet.cell(row=i, column=3).value] = []
    cities_in_states[cities_spreadsheet.cell(row=i, column=3).value].append(cities_spreadsheet.cell(row=i, column=1).value)
+cities_in_states.pop("MA")
 
 
-cities_and_counties = list(cities_in_states.values()) + list(counties_in_states.values())
-
+counties_and_cities = {}  
+for key in counties_in_states.keys():
+   counties_and_cities[key] = counties_in_states[key] + cities_in_states[key]
+   
 #cities in counties
 cities_in_counties = {}
 for i in range(2, 30411):
-   if not cities_spreadsheet.cell(row=i, column=6).value in cities_in_counties.keys(): 
-      cities_in_counties[cities_spreadsheet.cell(row=i, column=6).value] = []
-   cities_in_counties[cities_spreadsheet.cell(row=i, column=6).value].append(cities_spreadsheet.cell(row=i, column=1).value)
+   if not (cities_spreadsheet.cell(row=i, column=6).value + " County") in cities_in_counties.keys(): 
+      cities_in_counties[cities_spreadsheet.cell(row=i, column=6).value + " County"] = []
+   cities_in_counties[cities_spreadsheet.cell(row=i, column=6).value + " County"].append(cities_spreadsheet.cell(row=i, column=1).value)
+
+cities_list = []
+counties_list = []
+for i in range(2, 30411):
+   cities_list.append(cities_spreadsheet.cell(row=i, column=1).value)
+for i in range(2, 3145):
+   counties_list.append(counties_spreadsheet.cell(row=i, column=2).value)
+   
+state_crime_dictionary = {}
+state_crime_spreadsheet = state_crime_abbrev['OH']
+for i in range(6, 292):
+      state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value] = {}
+      state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Total Offences"] = median_house_prices_of_counties_spreadsheet.cell(row=i,column=3).value
+      state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Assault Offences"] = median_house_prices_of_counties_spreadsheet.cell(row=i,column=8).value
+      state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Homicide Offences"] = median_house_prices_of_counties_spreadsheet.cell(row=i,column=12).value
+      state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Kidnapping/Abduction"] = median_house_prices_of_counties_spreadsheet.cell(row=i,column=19).value
+      state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Burglary/Breaking and Entering"] = median_house_prices_of_counties_spreadsheet.cell(row=i,column=29).value
+      state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Fraud Offences"] = median_house_prices_of_counties_spreadsheet.cell(row=i,column=34).value
+      state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Identity Theft"] = median_house_prices_of_counties_spreadsheet.cell(row=i,column=40).value
+      state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Narcotic Offenses"] = median_house_prices_of_counties_spreadsheet.cell(row=i,column=55).value
+      state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Crime Rate"] = median_house_prices_of_counties_spreadsheet.cell(row=i,column=3).value / median_house_prices_of_counties_spreadsheet.cell(row=i,column=2).value
+
+#############################################
+
+state = ""
+actual_location = ""
+while state not in state_abbrev.keys() and state not in state_abbrev.values():
+   state = input("\nState: ")
+if state in state_abbrev.values():
+   state = state_unabbrev[state]
+   
+county_city = ""
+while county_city not in counties_list and county_city not in cities_list:
+   county_city = input("\nCounty/City: ")
+
+if county_city not in counties_list: 
+   for county in counties_in_states[state]:
+      for city in cities_in_counties[county]:
+         if county_city == city:
+            actual_location = county
+            break
+else:  
+   actual_location = county_city
+
+###############################################
+
+def DisplayData(location, actual_location):
+   string = "\n\n" + location + ("\n\nPopulation: {population}\nMedian House Price: ${median_house_price}\nMedian Rent Price: ${median_rent_price}").format(population=county_dictionary[actual_location]["Population"], median_house_price=county_dictionary[actual_location]["Median House Price"], median_rent_price=county_dictionary[actual_location]["Median Rent Price"])
+   return string
+
+#!!!!!!!!!!!!!!!!!!!!!!! output
+print(DisplayData(county_city + ", " + state, actual_location + ", " + state))
+#Population, Median House Price, Median Rent Price
+
+if county_city in state_crime_dictionary.keys():
+   state_crime_dictionary = {}
+   state_crime_spreadsheet = state_crime_abbrev['OH']
+   for i in range(6, 292):
+      if state_crime_spreadsheet.cell(row=i, column=2).value == county_city:
+         state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value] = {}
+         state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Total Offences"] = state_crime_spreadsheet.cell(row=i,column=3).value
+         state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Assault Offences"] = state_crime_spreadsheet.cell(row=i,column=8).value
+         state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Homicide Offences"] = state_crime_spreadsheet.cell(row=i,column=12).value
+         state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Kidnapping/Abduction"] = state_crime_spreadsheet.cell(row=i,column=19).value
+         state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Burglary/Breaking and Entering"] = state_crime_spreadsheet.cell(row=i,column=29).value
+         state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Fraud Offences"] = state_crime_spreadsheet.cell(row=i,column=34).value
+         state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Identity Theft"] = state_crime_spreadsheet.cell(row=i,column=40).value
+         state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Narcotic Offenses"] = state_crime_spreadsheet.cell(row=i,column=55).value
+         state_crime_dictionary[state_crime_spreadsheet.cell(row=i, column=2).value]["Crime Rate"] = state_crime_spreadsheet.cell(row=i,column=4).value / state_crime_spreadsheet.cell(row=i,column=3).value
+         
+         #!!!!!!!!!!!!!!!!!!!!!!! output
+         print([state_crime_dictionary[county_city]["Assault Offences"], state_crime_dictionary[county_city]["Homicide Offences"], state_crime_dictionary[county_city]["Kidnapping/Abduction"], state_crime_dictionary[county_city]["Burglary/Breaking and Entering"], state_crime_dictionary[county_city]["Fraud Offences"], state_crime_dictionary[county_city]["Identity Theft"], state_crime_dictionary[county_city]["Narcotic Offenses"]])
+         #assault, homicide, kidnappin, burglary, fraud, identity theft, narcotics
+
+         #!!!!!!!!!!!!!!!!!!!!!!! output
+         print([state_crime_dictionary[county_city]["Total Offences"], state_crime_dictionary[county_city]["Crime Rate"]])
+         #total offenses, crime rate
+         break
--- server/static/index.js
+++ server/static/index.js
@@ -1,101 +1,123 @@
-
-
+let jsuite;
+let jsuitedata = {};
 window.addEventListener("load", () => {
-  document.getElementById("location-selector").addEventListener("click", getLocation);
-
-  jSuites.dropdown(document.getElementById('dropdown'), {
-    url: '/v4/large',
+  jsuite = jSuites.dropdown(document.getElementById("dropdown"), {
+    url: "/v4/large",
     autocomplete: true,
     lazyLoading: true,
     multiple: false,
-    width: '100vh',
-    placeholder: 'Enter a state to get started',
-  data: ["no", "try again", "another city"]
+    width: "100vh",
+    placeholder: "Enter a state to get started",
+    data: [],
   });
 
-  document.body.addEventListener("keydown", function(key) {
-    const index = Array.from(document.querySelectorAll("input")).indexOf(document.activeElement)
-
-    if (index >= 0) {
-      const start = this.selectionStart
-
-    // Get last typed character
-    console.log(this, this.value)
-
-      sendQueryReq({type: "state", info: Array.from(document.querySelectorAll("input"))[index].value + key.key})
-    }
-  })
-
   function sendQueryReq(data) {
-      console.log(data)
-      $.ajax({
-        type: "POST",
-        contentType: "application/json",   
-        url: window.location.href.substring( 0, window.location.href.lastIndexOf( "/" ) + 1) + "autocomplete",
-        data: JSON.stringify(data),
-        success: function(response) {
-            console.log(response)
-            console.log(response[0])
-        },
+    $.ajax({
+      type: "POST",
+      contentType: "application/json",
+      url:
+        window.location.href.substring(
+          0,
+          window.location.href.lastIndexOf("/") + 1
+        ) + "autocomplete",
+      data: JSON.stringify(data),
+      success: function (response) {
+        jsuite = jSuites.dropdown(document.getElementById("dropdown"), {
+          url: "/v4/large",
+          autocomplete: true,
+          lazyLoading: true,
+          multiple: false,
+          width: "100vh",
+          placeholder: "Enter a state to get started",
+          data: response,
+        });
 
-        error: function(jqXHR, textStatus, errorThrown)
-        {
-            console.log("Error\n" + errorThrown, jqXHR)
-        },
-    })
+        let debounce = false
+        let selectedItem
+
+        console.log(document.getElementsByClassName("jdropdown-item"))
+
+        Array.from(document.getElementsByClassName("jdropdown-item")).forEach((x) =>  {
+          console.log(x)
+          function checkForChanges() {
+            console.log(x.classList.contains("jdropdown-selected"))
+          if (x.classList.contains("jdropdown-selected")) {
+            selectedItem = x
+
+            document.querySelector(".select-location").style.transition = "all 500ms"
+            document.querySelector(".or").style.transition = "all 500ms"
+            document.querySelector(".or").style.opacity = 0
+            document.querySelector(".select-location").style.opacity = 0
+          }else{
+          setTimeout(checkForChanges, 500);
+          }
+          
+        }
+        checkForChanges()
+        })
+
+
+      },
+    });
+
+    
   }
-
-  function getLocation(){
+function getLocation() {
       const options = {
-          enableHighAccuracy: true,
-          timeout: 5000,
-          maximumAge: 0
-        };
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      };
 
       function success(pos) {
-          const crd = pos.coords;
-          console.log('Your current position is:');
-          console.log('Latitude : ${crd.latitude}');
-          console.log('Longitude: ${crd.longitude}');
-          console.log('More or less ${crd.accuracy} meters.');
+        const crd = pos.coords;
+        console.log(`Your current position is:`);
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
 
-          const geocoder = new google.maps.Geocoder();
+        const geocoder = new google.maps.Geocoder();
 
-          const latlng = {
-              lat: parseFloat(crd.latitude),
-              lng: parseFloat(crd.longitude),
-            };
+        const latlng = {
+          lat: parseFloat(crd.latitude),
+          lng: parseFloat(crd.longitude),
+        };
 
-            geocoder
-              .geocode({ location: latlng })
-              .then((response) => {
-                if (response.results[0]) {
-                  console.log(response.results[4].formatted_address)
-                  console.log(response.results[6].formatted_address)
+        geocoder.geocode({ location: latlng }).then((response) => {
+          if (response.results[0]) {
+            console.log(response.results[4].formatted_address);
+            console.log(response.results[6].formatted_address);
 
-                  let res
-                  let comma = 0
-                  for (let i = 0; i<response.results[6].formatted_address.length; i++) {
-                      if (response.results[6].formatted_address[i] == ",") {
-                          comma++
-                      }
+            let res;
+            let comma = 0;
+            for (
+              let i = 0;
+              i < response.results[6].formatted_address.length;
+              i++
+            ) {
+              if (response.results[6].formatted_address[i] == ",") {
+                comma++;
+              }
 
-                      if (comma == 2) {
-                          res = response.results[6].formatted_address.substring(0, i)
-                          console.log(i)
-                          break
-                      }
-                  }
+              if (comma == 2) {
+                res = response.results[6].formatted_address.substring(0, i);
+                console.log(i);
+                break;
+              }
+            }
 
-                  console.log(res)
-                }
-              })
+            console.log(res);
+          }
+        });
       }
 
       function error(err) {
-      console.warn('ERROR(${err.code}): ${err.message}');
+        console.warn("ERROR(${err.code}): ${err.message}");
       }
 
       navigator.geolocation.getCurrentPosition(success, error, options);
-  }
-})
+    }
+    sendQueryReq({type: "state"})
+
+  document.getElementById("location-selector").addEventListener("click", getLocation);
+});
